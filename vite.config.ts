3ec3e480d0b5235e46bc5a28f26e736acd7f0d98
Topo A. Morolong <cse21-055@thuto.bac.ac.bw>
2025-08-05 11:11:18 +0200

    adding dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()], 
+
+  server: {
+    port: 3000,
+    // exit if port 3000 is in use (to avoid CORS errors; server expects port 3000)
+    //strict: true,
+  },
+}) 
+
