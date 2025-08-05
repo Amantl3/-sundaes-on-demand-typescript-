fec55bbef32e1c679fed046d9a4d3dcb05eaf96a
Topo A. Morolong <cse21-055@thuto.bac.ac.bw>
2025-08-05 15:50:44 +0200

    Complite file structure

---
+++ Task-Allocation.md
@@ -0,0 +1,61 @@
+# Task for Converting Sundae-on-demand  to TypeScript
+
+    tasks are allocated based on the pair programming concepts: 
+    Driver, navigator, reviewer
+
+# Tasks
+
+### Entry
+ A Role is held for the duration of red and green phase.
+
+###### Scoop 
+ - Driver: Amantle
+ - Navigator: Topo
+ - Reviewer: Pusetso
+
+###### Topping 
+ - Driver: Topo
+ - Navigator: Pusetso
+ - Reviewer: Amantle
+
+###### OrderEntry
+ - Driver: Pusetso
+ - Navigator: Amantle
+ - Reviewer: Topo
+
+###### Options 
+ - Driver: Amantle
+ - Navigator:  Topo
+ - Reviewer: Pusetso
+
+
+### Summary
+
+###### OrderSummary 
+ - Driver: Topo
+ - Navigator:  Amantle
+ - Reviewer: Pusetso
+
+###### SummaryForm 
+ - Driver: Topo
+ - Navigator:  Amantle
+ - Reviewer: Pusetso
+
+---
+### Confirmation
+
+###### OrderConfirmation 
+ - Driver: Pusetso
+ - Navigator: Topo
+ - Reviewer:  Amantle
+---
+### Common, Mocks, Utilities
+ - Driver: Amantle
+ - Navigator: Topo
+ - Reviewer: Pusetso
+---
+### OrderPhases 
+ - Driver: Topo
+ - Navigator: Amantle 
+ - Reviewer: Pusetso
+---
--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,14 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint, { plugin } from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import jestDom from "eslint-plugin-jest-dom";
+import testingLibrary from "eslint-plugin-testing-library";
+import vitest from "eslint-plugin-vitest";
 
 export default tseslint.config([
+  
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
@@ -17,7 +21,19 @@ export default tseslint.config([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: { ...globals.browser, ...vitest.environments.env.globals },
     },
+  
+  rules: {
+    ...jestDom.configs.recommended.rules,
+    ...testingLibrary.configs.react.rules,
+    ...vitest.configs.recommended.rules,
   },
+  plugins: {
+    "jest-dom": jestDom,
+    "testing-library": testingLibrary,
+    "vitest": vitest,
+  }
+
+  }, 
 ])
